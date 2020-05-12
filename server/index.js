bf0150922554b4448bcf9a712c9e8a6d484f8954
Breg Bromley <bregbromley@gmail.com>
2020-05-12 13:29:43 -0600

    reconfigured to use local server get requests

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 
@@ -16,6 +17,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+app.get('/rooms/:roomID/reviews', (req, res) => {
+  axios.get('http://localhost:3001/rooms/2/reviews')
+  .then(result => res.status(200).send(result.data))
+  .catch(err => console.log(err));
+});
+app.get('/rooms/:roomID/reviews/:phrase', (req, res) => {
+  axios.get(`http://localhost:3001/rooms/2/reviews/${req.params.phrase}`)
+  .then(result => res.status(200).send(result.data))
+  .catch(err => console.log(err));
+});
+app.get('/image', (req, res) => {
+  axios.get(`http://localhost:3003/image`)
+  .then(result => res.status(200).send(result.data))
+  .catch(err => console.log(err));
+});
+app.get('/rooms', (req, res) => {
+  axios.get(`http://localhost:3004/rooms`)
+  .then(result => res.status(200).send(result.data))
+  .catch(err => console.log(err));
+});
+app.get('/data', (req, res) => {
+  axios.get(`http://localhost:3002/data`)
+  .then(result => res.status(200).send(result.data))
+  .catch(err => console.log(err));
+});
 
 
 app.listen(3000, () => {
